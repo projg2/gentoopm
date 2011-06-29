28e30a0f287fce179c00af1b896f72cd8151afaa
Michał Górny <mgorny@gentoo.org>
2011-06-29 22:03:17 +0200

    Support getting any PM from a list.

--- gentoopm/submodules.py
+++ gentoopm/submodules.py
@@ -24,3 +24,18 @@ def get_pm(pm_name):
 	modname, clsname = supported_pms[pm_name]
 	mod = __import__(modname, fromlist=[clsname], level=-1)
 	return getattr(mod, clsname)()
+
+def get_any_pm(pm_list):
+	"""
+	Get the first working PackageManager from the pm_list. This function will
+	try to import them in order and either return the first succeeding or raise
+	an exception if all of them fail.
+	"""
+
+	for pm_name in pm_list:
+		try:
+			return get_pm(pm_name)
+		except Exception:
+			pass
+
+	raise Exception('None of the requested Package Managers is available.')
