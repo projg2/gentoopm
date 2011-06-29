4421dafa3b68f001399d130a83afe4cfa70ced17
Michał Górny <mgorny@gentoo.org>
2011-06-29 22:06:57 +0200

    Add a convenience function to get the best PM available.

--- gentoopm/__init__.py
+++ gentoopm/__init__.py
@@ -4,3 +4,15 @@
 # Released under the terms of the 2-clause BSD license.
 
 PV = '0'
+
+def get_package_manager():
+	"""
+	Get the PackageManager instance for the best package manager available.
+	Takes user preferences into consideration. Raises an exception if no PM
+	could be found.
+	"""
+
+	from gentoopm.preferences import get_preferred_pms
+	from gentoopm.submodules import get_any_pm
+
+	return get_any_pm(get_preferred_pms())
