b8c2770b86cec71c3b85af6fbd85635c304ca061
Michał Górny <mgorny@gentoo.org>
2011-06-29 21:50:56 +0200

    Support getting user PM preferences.

---
+++ gentoopm/preferences.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#	vim:fileencoding=utf-8
+# (c) 2011 Michał Górny <mgorny@gentoo.org>
+# Released under the terms of the 2-clause BSD license.
+
+import os
+
+# portage comes last as it is often installed aside anyway
+default_preference_list = ('paludis', 'pkgcore', 'portage')
+
+def get_preferred_pms():
+	"""
+	Find out what are the user's PM preferences and return the preferred PM
+	names as an iterable, with the most preferred one coming first.
+	"""
+
+	ret = []
+	if 'PACKAGE_MANAGER' in os.environ:
+		ret.append(os.environ['PACKAGE_MANAGER'])
+	for pm in default_preference_list:
+		if pm not in ret:
+			ret.append(pm)
+	return ret
