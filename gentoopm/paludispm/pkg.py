b60e625fda3b207df59763c1a06c1a062690bda8
Michał Górny <mgorny@gentoo.org>
2011-07-04 15:33:43 +0200

    Paludis: support grabbing metadata (partially).

--- gentoopm/paludispm/pkg.py
+++ gentoopm/paludispm/pkg.py
@@ -3,7 +3,9 @@
 # (c) 2011 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
-from gentoopm.basepm.pkg import PMKeyedPackageDict, PMPackage
+import paludis
+
+from gentoopm.basepm.pkg import PMKeyedPackageDict, PMPackage, PMPackageMetadata
 from gentoopm.util import IterDictWrapper
 
 class PaludisCategory(PMKeyedPackageDict):
@@ -46,3 +48,24 @@ class PaludisID(PMPackage):
 	def __init__(self, pkg, parent):
 		self._pkg = pkg
 		PMPackage.__init__(self, str(pkg.version), parent)
+
+	@property
+	def metadata(self):
+		return PaludisMetadata(self._pkg)
+
+class PaludisMetadata(PMPackageMetadata):
+	def __init__(self, pkg):
+		self._pkg = pkg
+
+	def __getitem__(self, key):
+		m = self._pkg.find_metadata(key)
+		if m is None:
+			return ''
+		m = m.parse_value()
+		if isinstance(m, paludis.StringSetIterable) \
+				or isinstance(m, paludis.KeywordNameIterable):
+			return ' '.join([str(x) for x in m])
+		elif isinstance(m, paludis.AllDepSpec):
+			raise NotImplementedError('Parsing %s is not supported yet.' % key)
+		else:
+			return str(m)
