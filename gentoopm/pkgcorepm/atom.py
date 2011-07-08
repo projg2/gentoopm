d6ad1d84ffb0ba44a9f4fd5271d61c55f817873d
Michał Górny <mgorny@gentoo.org>
2011-07-08 09:18:52 +0200

    pkgcore: support restriction creation and matching.

--- gentoopm/pkgcorepm/atom.py
+++ gentoopm/pkgcorepm/atom.py
@@ -3,7 +3,13 @@
 # (c) 2011 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
+from pkgcore.util.parserestrict import parse_match
+
 from gentoopm.basepm.atom import PMAtom
 
 class PkgCoreAtom(PMAtom):
-	pass
+	def __init__(self, s):
+		self._r = parse_match(s)
+
+	def __contains__(self, pkg):
+		return self._r.match(pkg._pkg)
