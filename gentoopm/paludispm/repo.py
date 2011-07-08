094ce09c41677dc13db2de512213d0c49f066a8e
Michał Górny <mgorny@gentoo.org>
2011-07-08 22:45:22 +0200

    Paludis: make __iter__() a little more reusable.

--- gentoopm/paludispm/repo.py
+++ gentoopm/paludispm/repo.py
@@ -26,22 +26,28 @@ class PaludisRepository(PMRepository, PaludisPackageSet):
 		self._env = env
 		self._sorted = True
 
+	@property
+	def _gen(self):
+		return paludis.Generator.InRepository(self._repo.name)
+
+	@property
+	def _filt(self):
+		return paludis.Filter.All()
+
 	def __iter__(self):
 		enum = PaludisEnumID()
 		for i, p in enumerate(self._env[paludis.Selection.AllVersionsSorted(
-				paludis.FilteredGenerator(
-					paludis.Generator.InRepository(self._repo.name),
-					paludis.Filter.All()))]):
+				paludis.FilteredGenerator(self._gen, self._filt))]):
 			yield PaludisID(p, i, enum)
 
 class PaludisStackRepo(PaludisRepository):
-	def __iter__(self):
-		enum = PaludisEnumID()
-		for i, p in enumerate(self._env[paludis.Selection.AllVersionsSorted(
-				paludis.FilteredGenerator(
-					paludis.Generator.All(),
-					paludis.Filter.SupportsInstallAction()))]):
-			yield PaludisID(p, i, enum)
+	@property
+	def _gen(self):
+		return paludis.Generator.All()
+
+	@property
+	def _filt(self):
+		return paludis.Filter.SupportInstallAction()
 
 class PaludisLivefsRepository(PaludisRepository, PMEbuildRepository):
 	def __init__(self, repo_obj, env):
