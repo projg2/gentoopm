a15d5b2ad120d17a92df2da61300f1da6faa7e6b
Michał Górny <mgorny@gentoo.org>
2011-07-11 11:40:06 +0200

    Support forking a BashParser out of PMPackageEnvironment.

--- gentoopm/basepm/environ.py
+++ gentoopm/basepm/environ.py
@@ -7,6 +7,21 @@ import bz2
 
 from gentoopm.bash import get_any_bashparser
 
+def _load_bp(bp, path, open_func):
+	"""
+	Load a file onto a bash parser.
+
+	@param bp: the bash parser instance
+	@type bp: L{BashParser}
+	@param path: path to the environment file
+	@type path: string
+	@param open_func: open function for the file
+	@type open_func: func(path, mode)
+	"""
+	f = open_func(path)
+	bp.load_file(f)
+	f.close()
+
 class LazyBashParser(object):
 	"""
 	Lazily-initialized, shared bash parser wrapper.
@@ -15,14 +30,21 @@ class LazyBashParser(object):
 	_parser = None
 
 	def set_file(self, path, open_func):
+		"""
+		Switch the currently used environment file, if necessary.
+
+		@param path: path to the new environment file
+		@type path: string
+		@param open_func: open functions for the new file
+		@type open_func: func(path, mode)
+		"""
+
 		if self._curr_path == path:
 			return
 		self._curr_path = path
-		f = open_func(path)
 		if self._parser is None:
 			self._parser = get_any_bashparser()
-		self._parser.load_file(f)
-		f.close()
+		_load_bp(self._parser, path, open_func)
 
 	def __getitem__(self, k):
 		return self._parser[k]
@@ -41,13 +63,49 @@ class PMPackageEnvironment(object):
 	"""
 
 	def __init__(self, path, bzipped2 = False):
+		"""
+		Instantiate L{PMPackageEnvironment} accessor.
+
+		@param path: path to the environment file
+		@type path: string
+		@param bzipped2: whether to bunzip2 the file
+		@type bzipped2: bool
+		"""
 		self._path = path
 		self._open_func = bz2.BZ2File if bzipped2 else open
 
 	def __getitem__(self, k):
+		"""
+		Get the value of an environment key by name.
+
+		@param k: the key to access
+		@type k: string
+		@return: the environment variable value
+		@rtype: string
+		"""
 		_bp.set_file(self._path, self._open_func)
 		return _bp[k]
 
 	def copy(self, *keys):
+		"""
+		Get a number of environment keys as a dict.
+
+		@param keys: keys to access
+		@type keys: strings
+		@return: a dict of copied environment keys
+		@rtype: dict(string -> string)
+		"""
 		_bp.set_file(self._path, self._open_func)
 		return _bp.copy(*keys)
+
+	def fork(self):
+		"""
+		Fork the bash parser. In other words, return a completely separate
+		instance with the environment file loaded.
+
+		@return: forked L{BashParser} instance
+		@rtype: L{BashParser}
+		"""
+		bp = get_any_bashparser()
+		_load_bp(bp, self._path, self._open_func)
+		return bp
