d96f83f3a431c47a8c025e8978d07b5970ecf0ee
Michał Górny <mgorny@gentoo.org>
2011-07-15 09:11:20 +0200

    Portage: use match() / xmatch() for faster Atom matching.

--- gentoopm/portagepm/repo.py
+++ gentoopm/portagepm/repo.py
@@ -5,9 +5,14 @@
 
 import os.path
 
+import portage.exception as pe
+from portage.versions import catsplit
+
 from gentoopm.basepm.repo import PMRepositoryDict, PMEbuildRepository, \
 		PMRepository
-from gentoopm.portagepm.pkg import PortageCPV, PortageDBCPV, PortagePackageSet
+from gentoopm.portagepm.atom import PortageAtom, CompletePortageAtom
+from gentoopm.portagepm.pkg import PortageCPV, PortageDBCPV, PortagePackageSet, \
+		PortageFilteredPackageSet
 
 class PortageRepoDict(PMRepositoryDict):
 	def __iter__(self):
@@ -29,6 +34,38 @@ class PortageRepoDict(PMRepositoryDict):
 	def __init__(self, portdbapi):
 		self._dbapi = portdbapi
 
+class PortageFilteredDBRepo(PortageFilteredPackageSet):
+	def __init__(self, repo, atom):
+		self._dbapi = repo._dbapi
+		self._atom = atom._atom
+
+	@property
+	def _stringified_atom(self):
+		a = str(self._atom)
+		return a.replace('null/', '')
+
+	def __iter__(self):
+		a = self._stringified_atom
+		try:
+			it = self._dbapi.match(a)
+		except pe.AmbiguousPackageName as e:
+			for pkgcand in e.args[0]:
+				for p in PortageHackedFilteredDBRepo(self, pkgcand):
+					yield p
+		else:
+			for p in it:
+				yield PortageDBCPV(p, self._dbapi)
+
+class PortageHackedFilteredDBRepo(PortageFilteredDBRepo):
+	def __init__(self, repo, pkgcand):
+		cat = catsplit(pkgcand)[0]
+		self._atom = str(repo._atom).replace('null/', '%s/' % cat)
+		self._dbapi = repo._dbapi
+
+	@property
+	def _stringified_atom(self):
+		return self._atom
+
 class PortDBRepository(PortagePackageSet, PMRepository):
 	def __init__(self, dbapi):
 		self._dbapi = dbapi
@@ -36,6 +73,77 @@ class PortDBRepository(PortagePackageSet, PMRepository):
 	def __iter__(self):
 		for p in self._dbapi.cpv_all(): # XXX
 			yield PortageDBCPV(p, self._dbapi)
+
+	_filtered_subclass = PortageFilteredDBRepo
+	def filter(self, *args, **kwargs):
+		newargs = []
+		filt = None
+		for a in args:
+			if isinstance(a, basestring):
+				a = PortageAtom(a)
+			if isinstance(a, CompletePortageAtom) and filt is None:
+				filt = a
+			else:
+				newargs.append(a)
+
+		pset = self
+		if filt:
+			pset = self._filtered_subclass(pset, filt)
+		if newargs or kwargs:
+			pset = PortageFilteredPackageSet(pset, newargs, kwargs)
+		return pset
+
+class PortageFilteredRepo(PortageFilteredDBRepo):
+	def __init__(self, repo, atom):
+		PortageFilteredDBRepo.__init__(self, repo, atom)
+		self._name = repo.name
+		self._path = repo.path
+		self._prio = repo._repo.priority
+
+	def __iter__(self):
+		if self._atom.repo is not None:
+			if self._atom.repo != self._name:
+				return
+			a = self._stringified_atom
+		else:
+			a = '%s::%s' % (self._stringified_atom, self._name)
+
+		try:
+			it = self._dbapi.xmatch("match-all", a)
+		except pe.AmbiguousPackageName as e:
+			for pkgcand in e.args[0]:
+				for p in PortageHackedFilteredRepo(self, pkgcand):
+					yield p
+		else:
+			for p in it:
+				yield PortageCPV(p, self._dbapi, self._path, self._prio)
+
+class PortageHackedAtom(object):
+	def __init__(self, s, repo):
+		self._s = s
+		self._repo = repo
+
+	@property
+	def repo(self):
+		return self._repo
+
+	def __str__(self):
+		return self._s
+
+class PortageHackedFilteredRepo(PortageFilteredRepo):
+	def __init__(self, repo, pkgcand):
+		cat = catsplit(pkgcand)[0]
+		self._atom = PortageHackedAtom(
+				str(repo._atom).replace('null/', '%s/' % cat),
+				repo._atom.repo)
+		self._dbapi = repo._dbapi
+		self._path = repo._path
+		self._prio = repo._prio
+		self._name = repo._name
+
+	@property
+	def _stringified_atom(self):
+		return str(self._atom)
 
 class PortageRepository(PortDBRepository, PMEbuildRepository):
 	def __init__(self, repo_obj, portdbapi):
@@ -48,6 +156,8 @@ class PortageRepository(PortDBRepository, PMEbuildRepository):
 		for cp in self._dbapi.cp_all(trees = (path,)):
 			for p in self._dbapi.cp_list(cp, mytree = path):
 				yield PortageCPV(p, self._dbapi, path, prio)
+
+	_filtered_subclass = PortageFilteredRepo
 
 	@property
 	def name(self):
