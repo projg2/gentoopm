a2a66f13a99656802f0615c0549e29b8309d183d
Michał Górny <mgorny@gentoo.org>
2021-03-27 09:50:03 +0100

    pkgcore: Fix PORTAGE_CONFIGROOT support in newer versions
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- gentoopm/pkgcorepm/__init__.py
+++ gentoopm/pkgcorepm/__init__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 #	vim:fileencoding=utf-8
-# (c) 2017 Michał Górny <mgorny@gentoo.org>
+# (c) 2017-2021 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
+from distutils.version import StrictVersion
 import os
+import os.path
 
-from pkgcore import __version__ as pkgcore_version
+from pkgcore import __version__ as pkgcore_version_raw
 from pkgcore.config import load_config
 
 from ..basepm import PackageManager
@@ -14,18 +16,29 @@ from .atom import PkgCoreAtom
 from .config import PkgCoreConfig
 from .repo import PkgCoreRepoDict, PkgCoreInstalledRepo
 
+
+PKGCORE_VERSION = StrictVersion(pkgcore_version_raw)
+
+
 class PkgCorePM(PackageManager):
 	name = 'pkgcore'
 
 	@property
 	def version(self):
-		return pkgcore_version
+		return pkgcore_version_raw
 
 	def reload_config(self):
-		if (self.config_root != '' and (self.config_root !=
-				os.environ.get('PORTAGE_CONFIGROOT', ''))):
-			raise NotImplementedError('pkgcore supports only PORTAGE_CONFIGROOT')
-		c = load_config()
+		config_root = os.environ.get('PORTAGE_CONFIGROOT')
+		if self.config_root != '':
+			config_root = self.config_root
+		kwargs = {}
+		if config_root != '':
+			if PKGCORE_VERSION >= '0.11.7':
+				kwargs['location'] = os.path.join(
+						config_root, 'etc', 'portage')
+			else:
+				kwargs['configroot'] = config_root
+		c = load_config(**kwargs)
 		self._domain = c.get_default('domain')
 
 	@property
